6db86b462202ddefd062c72f117c83633a40becc
John Griffiths <j.davidgriffiths@gmail.com>
2022-07-03 21:06:21 -0400

    data download file

---
+++ data/download_ds000201_data.py
@@ -0,0 +1,29 @@
+
+
+## Setup Intructions
+
+!gdown https://drive.google.com/drive/folders/1rlh75dpjOJxeS0WZPojuzjAwl5OENWrV -O subjinfo --folder --remaining-ok
+
+!gdown https://drive.google.com/drive/folders/1cu6bCLNEp_YR-8C3zm_v2B7_lXQM9Zg0 -O ptseries --folder --remaining-ok
+
+!gdown https://drive.google.com/drive/folders/12os_7xC10QUwJANBJb-5CXG1Gg_9YUgQ  -O dwiconn --folder --remaining-ok
+```
+
+
+```python
+from kspw.pytorrww import Model_fitting,RNNWWD,plot_fit_parameters
+```
+
+```bash
+wget https://github.com/ThomasYeoLab/CBIG/raw/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/HCP/fslr32k/cifti/Schaefer2018_100Parcels_7Networks_order.dlabel.nii
+```
+
+cd ..
+
+
+```python
+from kspw.pytorrww import Model_fitting,RNNWWD,plot_fit_parameters
+```
+
+
+
